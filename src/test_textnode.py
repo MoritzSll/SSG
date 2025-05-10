8a944252f6be0d93fde5e4f607c7dfee8e731d07
Moritz Schalle <schallemoritz188@gmail.com>
2025-05-10 13:04:16 +0200

    text_node_to_html_node and tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode,TextType,text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -18,6 +18,26 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.LINK, "https:/blablabla.com")
         node2 = TextNode("This is a text node", TextType.LINK, "https:/blablabla.com")
         self.assertEqual(node,node2)
+    
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+    
+    def test_link(self):
+        node = TextNode("link to",TextType.LINK,"https:/blablabla.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag,"a")
+        self.assertEqual(html_node.value,"link to")
+        self.assertEqual(html_node.props,{"href":"https:/blablabla.com"})
+    
+    def test_img(self):
+        node = TextNode(None,TextType.IMAGE,"./src/img/chess")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag,"img")
+        self.assertEqual(html_node.value,None)
+        self.assertEqual(html_node.props,{"src":"./src/img/chess","alt":"error"})
 
 
 
